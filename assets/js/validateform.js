88a73e19f4594e56c9e1cb842d7e16894383f60d
PotapovDmitriy <lhfujy7477@gmail.com>
2019-12-09 00:13:48 +0500

    add preview

--- assets/js/validateform.js
+++ assets/js/validateform.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function () {
     console.log("submit")
-
     $('#submit-button').click( function(){
         if($('#news-text').val().length == 0){
             $('#news-text').css({
@@ -9,7 +8,6 @@ $(document).ready(function () {
                 'border-color': 'red'
             });
         }
-
         try{
             var file = document.getElementById('input-container').files[0]; 
             console.log(file.name);
@@ -17,10 +15,6 @@ $(document).ready(function () {
         }
         catch{
             console.log("null")
-            // console.log($('#input-container').val.length)
-            // $('#input-container__input-title').css({
-            //     "color": "red"
-            // })
         }
     })
 
@@ -44,51 +38,51 @@ $(document).ready(function () {
             if(file.type != "image/png"  && file.type != "image/jpeg"){
 
                 $('#file-name').html("Неверный формат файла");
-                $("#submit-button").prop('disabled', true);
-                // $("#input-container__input-title").css({
-                //     "color": "red"                  
-                // });
+                $("#submit-button").prop('disabled', true);                
                 $('#file-name').css({
                     "color": "red"                        
                 });
-                // $('#submit-button').css({
-                //     "background": "red" 
-                // });
-
             }else{
                 $('#file-name').html(file.name);
                 $("#submit-button").prop('disabled', false);
                 $('#file-name').css({
                     "color": "black"                        
                 });
-                // $('#submit-button').css({
-                //     'background': '#E5E31F'
-                // });
-                // preview(file);
+                preview(file)
             }
 
     });
+    $("#revoke-btn").click( function(){
+        
+        $('#preview-img').remove();
+        $('#file-name').css({
+            "display": "none"                       
+        });
+        $(this).css({
+            "display": "none"                       
+        });
+    });
     
 }); 
-// function preview(file) {
-//     var reader = new FileReader();
-//     reader.addEventListener('load', function(event) {
-//         var img = document.createElement('img');
-
-//         var itemPreview = itemPreviewTemplate.clone();
-
-//         itemPreview.find('#img-wrap img').attr('src', event.target.result);
-//         $("#img-wrap").css({
-//             "display":"inline"
-//         });
-//         itemPreview.data('id', file.name);
-
-//         imagesList.append(itemPreview);
-
-//         queue[file.name] = file;
-
-//     });
-//     reader.readAsDataURL(file);
-// }
+function preview(f) {
+    var reader = new FileReader();
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+        return function(e) {
+            // Render thumbnail.
+            var span = document.createElement('span');
+            span.innerHTML = ['<img id="preview-img" title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+            document.getElementById('img-wrap').insertBefore(span, null);
+            $('#preview-img').css({
+                "width": "50px"                        
+            });
+            $('#revoke-btn').css({
+                "display": "inline"                        
+            });
+        };
+    })(f);
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+}
 
 
